657220e4e8347241c655496dcac9436423bc1634
James MacAdie <james.macadie@telerealtrillium.com>
2014-05-30 15:20:49 +0100

    Added HTML for filters to fixtures page

--- fixtures/fixtures.html.php
+++ fixtures/fixtures.html.php
@@ -1,1 +1,58 @@
 <h1>Fixtures</h1>
+<div class="panel-group" id="accordionFilters">
+	<div class="panel panel-default">
+		<div class="panel-heading">
+			<h4 class="panel-title">
+			<a data-toggle="collapse" data-parent="#accordionFilters" href="#collapseFilters">
+			  Filters
+			</a>
+			</h4>
+		</div>
+		<div id="collapseFilters" class="panel-collapse collapse in">
+			<div class="panel-body">
+				<div class="checkbox">
+				  <label>
+					<input type="checkbox" id="ckbLockedMatches" value="" checked>
+					Exclude locked-down matches
+				  </label>
+				</div>
+				<div class="checkbox">
+				  <label>
+					<input type="checkbox" id="ckbLockedMatches" value="" checked>
+					Exclude predicted matches
+				  </label>
+				</div>
+				<div class="checkbox">
+				  <label>
+					<input type="checkbox" id="ckbGroupStage" value="" checked>
+					Group Stage
+				  </label>
+				</div>
+				<div class="checkbox">
+				  <label>
+					<input type="checkbox" id="ckbLockedMatches" value="" checked>
+					Last 16
+				  </label>
+				</div>
+				<div class="checkbox">
+				  <label>
+					<input type="checkbox" id="ckbLockedMatches" value="" checked>
+					Quarter-finals
+				  </label>
+				</div>
+				<div class="checkbox">
+				  <label>
+					<input type="checkbox" id="ckbLockedMatches" value="" checked>
+					Semi-finals
+				  </label>
+				</div>
+				<div class="checkbox">
+				  <label>
+					<input type="checkbox" id="ckbLockedMatches" value="" checked>
+					Final
+				  </label>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
