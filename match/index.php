c33ec21eefe22087c490435281f9233d6e492729
James MacAdie <james@macadie.co.uk>
2014-06-05 18:17:24 +0100

    First pass at putting together match page, very incomplete

--- match/index.php
+++ match/index.php
@@ -5,15 +5,14 @@ if (!isset($_SESSION)) session_start();
 // Make sure all relevant includes are loaded
 require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/includesfile.inc.php';
 
-// Call log in function to process any update requests from page
-userIsLoggedIn();
+//TODO:use posted match ID to grab relevent match data from DB
 
-// Set tab variable to indicate point to data tab
-$tab = 'details';
+// Set tab variable to indicate point to match page
+$tab = 'match';
 
 // Set Content pointer
-$content = $_SERVER['DOCUMENT_ROOT'] . '/details/details.html.php';
-$contentjs = $_SERVER['DOCUMENT_ROOT'] . '/details/details.js.php';
+$content = $_SERVER['DOCUMENT_ROOT'] . '/match/match.html.php';
+$contentjs = $_SERVER['DOCUMENT_ROOT'] . '/match/match.js.php';
 
 // Call main HTML page
 include $_SERVER['DOCUMENT_ROOT'] . '/includes/template.html.php';
