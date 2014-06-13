2af07e430942f0526034d8b5691e9a54afd9fd34
James MacAdie <james.macadie@telerealtrillium.com>
2014-06-13 11:40:48 +0100

    Added tables for the sub-stage tournaments

--- results/results.html.php
+++ results/results.html.php
@@ -1,60 +1,246 @@
 <h1>Tables</h1>
 <div id="messageTables"></div>
+<hr>
 <h3>
 	Overall
-	<button type="button" class="btn btn-default btn-sm" data-toggle="collapse" data-target="#tOverallCont">
+	<button type="button" class="btn btn-primary btn-xs" data-toggle="collapse" data-target="#tOverallCont">
 		hide / unhide table
 	</button>
 </h3>
-<div id="tOverallCont" class="table-responsive collapse in" data-pattern="priority-columns">
-	<table class="table table-small-font table-striped" id ="tOverall">
-		<thead>
-			<tr>
-				<th data-priority="1">Position</th>
-				<th>Player</th>
-				<th data-priority="2" class="text-center">Predicted</th>
-				<th data-priority="2" class="text-center">Correct Results</th>
-				<th data-priority="2" class="text-center">Exact Scores</th>
-				<th data-priority="1" class="text-center">Points</th>
-				<th data-priority="2" class="text-center">Points per Prediction</th>
-			</tr>
-		</thead>
-			<?php foreach ($resultLeague as $row): ?>
-			<tr>
-				<td><?php if($row['rankCount'] > 1) {
-						htmlout($row['rank'] . '=');
-					} else {
-						htmlout($row['rank']);
-					} ?></td>
-				<td><?php htmlout($row['name']); ?></td>
-				<td class="text-center"><?php if($row['submitted'] == 0) {
-						htmlout('-');
-					} else {
-						htmlout($row['submitted']);
-					} ?></td>
-				<td class="text-center"><?php if($row['results'] == 0) {
-						htmlout('-');
-					} else {
-						htmlout($row['results']);
-					} ?></td>
-				<td class="text-center"><?php if($row['scores'] == 0) {
-						htmlout('-');
-					} else {
-						htmlout($row['scores']);
-					} ?></td>
-				<td class="text-center"><strong><?php if($row['totalPoints'] == 0) {
-						htmlout('-');
-					} else {
-						htmlout($row['totalPoints']);
-					} ?></strong></td>
-				<td class="text-center"><?php if(($row['submitted'] == 0) || ($row['totalPoints'] == 0)) {
-						htmlout('-');
-					} else {
-						htmlout(round($row['totalPoints'] / $row['submitted'], 1));
-					} ?></td>
-			</tr>
-			<?php endforeach; ?>
-		<tbody>	
-		</tbody>
-	</table>
+<div id="tOverallCont" class="collapse in">
+	<div class="table-responsive" data-pattern="priority-columns">
+		<table class="table table-small-font table-striped">
+			<thead>
+				<tr>
+					<th data-priority="1">Position</th>
+					<th>Player</th>
+					<th data-priority="2" class="text-center">Predicted</th>
+					<th data-priority="2" class="text-center">Correct Results</th>
+					<th data-priority="2" class="text-center">Exact Scores</th>
+					<th data-priority="1" class="text-center">Points</th>
+					<th data-priority="2" class="text-center">Points per Prediction</th>
+				</tr>
+			</thead>
+				<?php foreach ($resultLeague as $row): ?>
+				<tr>
+					<td><?php if($row['rankCount'] > 1) {
+							htmlout($row['rank'] . '=');
+						} else {
+							htmlout($row['rank']);
+						} ?></td>
+					<td><?php htmlout($row['name']); ?></td>
+					<td class="text-center"><?php if($row['submitted'] == 0) {
+							htmlout('-');
+						} else {
+							htmlout($row['submitted']);
+						} ?></td>
+					<td class="text-center"><?php if($row['results'] == 0) {
+							htmlout('-');
+						} else {
+							htmlout($row['results']);
+						} ?></td>
+					<td class="text-center"><?php if($row['scores'] == 0) {
+							htmlout('-');
+						} else {
+							htmlout($row['scores']);
+						} ?></td>
+					<td class="text-center"><strong><?php if($row['totalPoints'] == 0) {
+							htmlout('-');
+						} else {
+							htmlout($row['totalPoints']);
+						} ?></strong></td>
+					<td class="text-center"><?php if(($row['submitted'] == 0) || ($row['totalPoints'] == 0)) {
+							htmlout('-');
+						} else {
+							htmlout(round($row['totalPoints'] / $row['submitted'], 1));
+						} ?></td>
+				</tr>
+				<?php endforeach; ?>
+			<tbody>	
+			</tbody>
+		</table>
+	</div>
+</div>
+<hr>
+<h3>
+	Group Stages
+	<button type="button" class="btn btn-primary btn-xs" data-toggle="collapse" data-target="#tGSCont">
+		hide / unhide table
+	</button>
+</h3>
+<div id="tGSCont" class="collapse">
+	<div class="table-responsive" data-pattern="priority-columns">
+		<table class="table table-small-font table-striped">
+			<thead>
+				<tr>
+					<th data-priority="1">Position</th>
+					<th>Player</th>
+					<th data-priority="2" class="text-center">Predicted</th>
+					<th data-priority="2" class="text-center">Correct Results</th>
+					<th data-priority="2" class="text-center">Exact Scores</th>
+					<th data-priority="1" class="text-center">Points</th>
+					<th data-priority="2" class="text-center">Points per Prediction</th>
+				</tr>
+			</thead>
+				<?php foreach ($resultGSLeague as $row): ?>
+				<tr>
+					<td><?php if($row['rankCount'] > 1) {
+							htmlout($row['rank'] . '=');
+						} else {
+							htmlout($row['rank']);
+						} ?></td>
+					<td><?php htmlout($row['name']); ?></td>
+					<td class="text-center"><?php if($row['submitted'] == 0) {
+							htmlout('-');
+						} else {
+							htmlout($row['submitted']);
+						} ?></td>
+					<td class="text-center"><?php if($row['results'] == 0) {
+							htmlout('-');
+						} else {
+							htmlout($row['results']);
+						} ?></td>
+					<td class="text-center"><?php if($row['scores'] == 0) {
+							htmlout('-');
+						} else {
+							htmlout($row['scores']);
+						} ?></td>
+					<td class="text-center"><strong><?php if($row['totalPoints'] == 0) {
+							htmlout('-');
+						} else {
+							htmlout($row['totalPoints']);
+						} ?></strong></td>
+					<td class="text-center"><?php if(($row['submitted'] == 0) || ($row['totalPoints'] == 0)) {
+							htmlout('-');
+						} else {
+							htmlout(round($row['totalPoints'] / $row['submitted'], 1));
+						} ?></td>
+				</tr>
+				<?php endforeach; ?>
+			<tbody>	
+			</tbody>
+		</table>
+	</div>
+</div>
+<hr>
+<h3>
+	Last 16
+	<button type="button" class="btn btn-primary btn-xs" data-toggle="collapse" data-target="#tL16Cont">
+		hide / unhide table
+	</button>
+</h3>
+<div id="tL16Cont" class="collapse">
+	<div class="table-responsive" data-pattern="priority-columns">
+		<table class="table table-small-font table-striped">
+			<thead>
+				<tr>
+					<th data-priority="1">Position</th>
+					<th>Player</th>
+					<th data-priority="2" class="text-center">Predicted</th>
+					<th data-priority="2" class="text-center">Correct Results</th>
+					<th data-priority="2" class="text-center">Exact Scores</th>
+					<th data-priority="1" class="text-center">Points</th>
+					<th data-priority="2" class="text-center">Points per Prediction</th>
+				</tr>
+			</thead>
+				<?php foreach ($resultL16League as $row): ?>
+				<tr>
+					<td><?php if($row['rankCount'] > 1) {
+							htmlout($row['rank'] . '=');
+						} else {
+							htmlout($row['rank']);
+						} ?></td>
+					<td><?php htmlout($row['name']); ?></td>
+					<td class="text-center"><?php if($row['submitted'] == 0) {
+							htmlout('-');
+						} else {
+							htmlout($row['submitted']);
+						} ?></td>
+					<td class="text-center"><?php if($row['results'] == 0) {
+							htmlout('-');
+						} else {
+							htmlout($row['results']);
+						} ?></td>
+					<td class="text-center"><?php if($row['scores'] == 0) {
+							htmlout('-');
+						} else {
+							htmlout($row['scores']);
+						} ?></td>
+					<td class="text-center"><strong><?php if($row['totalPoints'] == 0) {
+							htmlout('-');
+						} else {
+							htmlout($row['totalPoints']);
+						} ?></strong></td>
+					<td class="text-center"><?php if(($row['submitted'] == 0) || ($row['totalPoints'] == 0)) {
+							htmlout('-');
+						} else {
+							htmlout(round($row['totalPoints'] / $row['submitted'], 1));
+						} ?></td>
+				</tr>
+				<?php endforeach; ?>
+			<tbody>	
+			</tbody>
+		</table>
+	</div>
+</div>
+<hr>
+<h3>
+	Semi-Finals, Quarter-Finals, 3<sup>rd</sup> 4<sup>th</sup> Place Play-off & Final
+	<button type="button" class="btn btn-primary btn-xs" data-toggle="collapse" data-target="#tRCont">
+		hide / unhide table
+	</button>
+</h3>
+<div id="tRCont" class="collapse">
+	<div class="table-responsive" data-pattern="priority-columns">
+		<table class="table table-small-font table-striped">
+			<thead>
+				<tr>
+					<th data-priority="1">Position</th>
+					<th>Player</th>
+					<th data-priority="2" class="text-center">Predicted</th>
+					<th data-priority="2" class="text-center">Correct Results</th>
+					<th data-priority="2" class="text-center">Exact Scores</th>
+					<th data-priority="1" class="text-center">Points</th>
+					<th data-priority="2" class="text-center">Points per Prediction</th>
+				</tr>
+			</thead>
+				<?php foreach ($resultRLeague as $row): ?>
+				<tr>
+					<td><?php if($row['rankCount'] > 1) {
+							htmlout($row['rank'] . '=');
+						} else {
+							htmlout($row['rank']);
+						} ?></td>
+					<td><?php htmlout($row['name']); ?></td>
+					<td class="text-center"><?php if($row['submitted'] == 0) {
+							htmlout('-');
+						} else {
+							htmlout($row['submitted']);
+						} ?></td>
+					<td class="text-center"><?php if($row['results'] == 0) {
+							htmlout('-');
+						} else {
+							htmlout($row['results']);
+						} ?></td>
+					<td class="text-center"><?php if($row['scores'] == 0) {
+							htmlout('-');
+						} else {
+							htmlout($row['scores']);
+						} ?></td>
+					<td class="text-center"><strong><?php if($row['totalPoints'] == 0) {
+							htmlout('-');
+						} else {
+							htmlout($row['totalPoints']);
+						} ?></strong></td>
+					<td class="text-center"><?php if(($row['submitted'] == 0) || ($row['totalPoints'] == 0)) {
+							htmlout('-');
+						} else {
+							htmlout(round($row['totalPoints'] / $row['submitted'], 1));
+						} ?></td>
+				</tr>
+				<?php endforeach; ?>
+			<tbody>	
+			</tbody>
+		</table>
+	</div>
 </div>
