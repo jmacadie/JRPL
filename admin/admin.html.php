50c6d1acd0c096359ae0253deb2a705d6006c72a
James MacAdie (server) <james@macadie.co.uk>
2015-08-13 13:32:26 +0100

    Fix indentation and long lines
    One of many commits because doing one file at a time. Actually split this file
    into multiple bits as got bored of doing the html files (it's hard work!)

--- admin/admin.html.php
+++ admin/admin.html.php
@@ -3,7 +3,9 @@
   <div class="panel panel-default">
     <div class="panel-heading">
       <h4 class="panel-title">
-        <a data-toggle="collapse" data-parent="#accordionTournamentRoles" href="#collapseTournamentRoles">
+        <a data-toggle="collapse"
+           data-parent="#accordionTournamentRoles"
+           href="#collapseTournamentRoles">
           Set Tournament Roles
         </a>
       </h4>
@@ -14,42 +16,60 @@
           <div class="panel panel-default">
             <div class="panel-heading">
               <h4 class="panel-title">
-                <a data-toggle="collapse" data-parent="#accordionTRGroup" href="#collapseTRGroup">
+                <a data-toggle="collapse"
+                   data-parent="#accordionTRGroup"
+                   href="#collapseTRGroup">
                   Group Stages
                 </a>
               </h4>
             </div>
             <div id="collapseTRGroup" class="panel-collapse collapse">
               <div class="panel-body">
-                <?php foreach ($arrTournamentRoles as $row) : ?>
-                  <?php if($row['stage'] == 'Group Stages') : ?>
-                    <div class="row">
-                    <form class="form-inline" role="form">
-                      <div class="col-sm-5 text-left">
-                        <label for="tr-<?php echo($row['tournamentRoleID']); ?>"><?php echo($row['tournamentRole']); ?>:</label>
-                        <?php $flag = ($row['teamS'] === NULL) ? 'tmp' : strtolower($row['teamS']); ?>
-                        <img alt="<?php echo($row['team']); ?>" class="flag" src="../assets/img/flags/<?php echo($flag); ?>.png">
-                        <span class="tr-team"><?php echo($row['team']); ?></span>
-                      </div>
-                      <div class="col-sm-5 text-left">
-                        <select class="form-control" id="tr-<?php echo($row['tournamentRoleID']); ?>">
-                          <option value="0" <?php if($row['teamID'] === NULL) echo('selected'); ?>> - - - </option>
-                          <?php foreach ($row['selectTeam'] as $team) : ?>
-                            <?php if($team['id'] != NULL) : ?>
-                              <option value = "<?php echo($team['id']); ?>" <?php if($team['id'] === $row['teamID']) echo('selected'); ?>><?php echo($team['name']); ?></option>
-                            <?php endif; ?>
-                          <?php endforeach; ?>
-                        </select>
-                      </div>
-                      <div class="col-sm-2 text-center">
-                        <button type="submit" class="btn btn-primary">Update</button>
-                      </div>
-                    </form>
-                    </div>
-                    <div class="row"><div class="messageTR col-sm-12"></div></div>
-                    <hr>
-                  <?php endif; ?>
-                <?php endforeach; ?>
+<?php foreach ($arrTournamentRoles as $row) : ?>
+  <?php if($row['stage'] == 'Group Stages') : ?>
+                <div class="row">
+                <form class="form-inline" role="form">
+                  <div class="col-sm-5 text-left">
+                    <label for="tr-<?php echo($row['tournamentRoleID']); ?>">
+                      <?php echo($row['tournamentRole']); ?>:
+                    </label>
+    <?php $flag = ($row['teamS'] === NULL) ? 'tmp' : strtolower($row['teamS']); ?>
+                    <img alt="<?php echo($row['team']); ?>"
+                         class="flag"
+                         src="../assets/img/flags/<?php echo($flag); ?>.png">
+                    <span class="tr-team"><?php echo($row['team']); ?></span>
+                  </div>
+                  <div class="col-sm-5 text-left">
+                    <select class="form-control"
+                            id="tr-<?php echo($row['tournamentRoleID']); ?>">
+                      <option value="0"
+                              <?php if($row['teamID'] === NULL) echo('selected'); ?>>
+                        - - -
+                      </option>
+    <?php foreach ($row['selectTeam'] as $team) : ?>
+      <?php if($team['id'] != NULL) : ?>
+                      <option value = "<?php echo($team['id']); ?>"
+                              <?php if($team['id'] === $row['teamID'])
+                                      echo('selected'); ?>>
+                        <?php echo($team['name']); ?>
+                      </option>
+      <?php endif; ?>
+    <?php endforeach; ?>
+                    </select>
+                  </div>
+                  <div class="col-sm-2 text-center">
+                    <button type="submit" class="btn btn-primary">
+                      Update
+                    </button>
+                  </div>
+                </form>
+                </div>
+                <div class="row">
+                  <div class="messageTR col-sm-12"></div>
+                </div>
+                <hr>
+  <?php endif; ?>
+<?php endforeach; ?>
               </div>
             </div>
           </div>
