241c08885a64b5fbf692111115aecec2b31ed66d
James MacAdie (server) <james@macadie.co.uk>
2015-08-13 12:59:57 +0100

    Fix indentation and long lines
    Doing this one file at a time so one of a series

--- admin/getTournamentRoles.php
+++ admin/getTournamentRoles.php
@@ -16,48 +16,56 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/includesfile.inc.php';
 include $_SERVER['DOCUMENT_ROOT'] . '/includes/db.inc.php';
 
 // Query to pull tournament role data from DB
-$sql = "SELECT
-      tr.`TournamentRoleID`,
-      s.`Name` AS `Stage`,
-      tr.`Name` AS `TournamentRole`,
-      t.`TeamID`,
-      t.`Name` AS `Team`,
-      t.`ShortName` AS `TeamS`,
-      IFNULL(gt.`TeamID`, st.`TeamID`) AS `SelectTeamID`,
-      IFNULL(gt.`Name`, st.`Name`) AS `SelectTeam`
+$sql =
+"SELECT
+  tr.`TournamentRoleID`,
+  s.`Name` AS `Stage`,
+  tr.`Name` AS `TournamentRole`,
+  t.`TeamID`,
+  t.`Name` AS `Team`,
+  t.`ShortName` AS `TeamS`,
+  IFNULL(gt.`TeamID`, st.`TeamID`) AS `SelectTeamID`,
+  IFNULL(gt.`Name`, st.`Name`) AS `SelectTeam`
 
-    FROM `TournamentRole` tr
+FROM `TournamentRole` tr
 
-      INNER JOIN `Stage` s ON
-        tr.`StageID` = s.`StageID`
+  INNER JOIN `Stage` s ON
+    tr.`StageID` = s.`StageID`
 
-      LEFT JOIN `Team` t ON
-        t.`TeamID` = tr.`TeamID`
+  LEFT JOIN `Team` t ON
+    t.`TeamID` = tr.`TeamID`
 
-      LEFT JOIN `TournamentRole` trg ON
-        trg.`FromGroupID` = tr.`FromGroupID` AND trg.`StageID` = 1
-      LEFT JOIN `Team` gt ON
-        gt.`TeamID` = trg.`TeamID`
+  LEFT JOIN `TournamentRole` trg ON
+    trg.`FromGroupID` = tr.`FromGroupID` AND trg.`StageID` = 1
 
-      LEFT JOIN
-        (SELECT `MatchID`,`HomeTeamID` AS `TeamID` FROM `Match` UNION ALL
-        SELECT `MatchID`,`AwayTeamID` AS `TeamID` FROM `Match`) m ON
-        m.`MatchID` = tr.`FromMatchID`
-      LEFT JOIN `TournamentRole` trt ON
-        trt.`TournamentRoleID` = m.`TeamID`
-      LEFT JOIN `Team` st ON
-        st.`TeamID` = trt.`TeamID`
+  LEFT JOIN `Team` gt ON
+    gt.`TeamID` = trg.`TeamID`
 
-    ORDER BY
-      s.`StageID` ASC,
-      tr.`TournamentRoleID` ASC,
-      gt.`TeamID` ASC,
-      st.`TeamID` ASC";
+  LEFT JOIN
+    (SELECT `MatchID`,`HomeTeamID` AS `TeamID` FROM `Match` UNION ALL
+    SELECT `MatchID`,`AwayTeamID` AS `TeamID` FROM `Match`) m ON
+    m.`MatchID` = tr.`FromMatchID`
+
+  LEFT JOIN `TournamentRole` trt ON
+    trt.`TournamentRoleID` = m.`TeamID`
+
+  LEFT JOIN `Team` st ON
+    st.`TeamID` = trt.`TeamID`
+
+ORDER BY
+  s.`StageID` ASC,
+  tr.`TournamentRoleID` ASC,
+  gt.`TeamID` ASC,
+  st.`TeamID` ASC;";
 
 // Run query and handle any failure
 $result = mysqli_query($link, $sql);
 if (!$result) {
-  $error = 'Error fetching tournament roles: <br />' . mysqli_error($link) . '<br /><br />' . $sql;
+  $error =
+    'Error fetching tournament roles: <br />' .
+    mysqli_error($link) .
+    '<br /><br />' .
+    $sql;
 
   header('Content-type: application/json');
   $arr = array('result' => 'No', 'message' => $error);
@@ -74,7 +82,8 @@ while($row = mysqli_fetch_assoc($result)) {
   // Check if we're onto a new Tournament Role
   if ($row['TournamentRole'] != $tr) {
 
-    // If $tr is set then must have already done a loop so push completed TR array onto output array
+    // If $tr is set then must have already done a loop so push completed TR
+    // array onto output array
     if ($tr != '') {
       $arrTROut ['selectTeam'] = $arrTRTOut;
       $arrTournamentRoles[] = $arrTROut;
@@ -98,7 +107,8 @@ while($row = mysqli_fetch_assoc($result)) {
   }
 
   // Push the team onto the select team array
-  $arrTRTOut[] = array('id' => $row['SelectTeamID'], 'name' => $row['SelectTeam']);
+  $arrTRTOut[] = array('id' => $row['SelectTeamID'],
+                       'name' => $row['SelectTeam']);
 }
 
 // Push final TR array onto output array
