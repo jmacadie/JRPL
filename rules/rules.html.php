dd9d56de0dc32022e2d5d15e13fc332f6617e786
James MacAdie (server) <james@macadie.co.uk>
2015-08-17 17:38:08 +0100

    Updated Rules page
    Re-wrote the rules page to cover the rules for the Rugby, as opposed to football
    previously

--- rules/rules.html.php
+++ rules/rules.html.php
@@ -2,27 +2,87 @@
 <div class="alert alert-info">I've got a four lettered word for you...</div>
 <h2>How does it work?</h2>
 <ul>
-  <li>Predict the exact score in every match in the 2014 World Cup</li>
+  <li>Predict the exact score in every match in the 2015 Rugby World Cup</li>
   <li>Change your prediction as many times as you like before the match</li>
-  <li>Matches will be locked down from further prediction <strong>30 minutes</strong> before kick-off</li>
-  <li>The system will send an e-mail at lock-down saying what every player predicted for that match</li>
-  <li>After the match is over, and someone has posted the result, the predictions will be scores as follows:
+  <li>Matches will be locked down from further prediction
+    <strong>30 minutes</strong> before kick-off</li>
+  <li>The system will send an e-mail at lock-down saying what every player
+    predicted for that match</li>
+  <li>After the match is over, and someone has posted the result, the
+    predictions will be scored as follows:<br />
+    <br />
+    <strong><u>Result</u></strong>
     <ul>
-      <li><strong>3 points</strong> for the correct result (e.g. Brazil Win, Draw, Croatia Win) and scoreline, <i>or</i></li>
-      <li><strong>1 point</strong> for the correct result but the wrong scoreline, <i>or</i></li>
+      <li><strong>3 points</strong> for the correct result (e.g. England Win,
+        Draw, Fiji Win)</li>
       <li><strong>0 points</strong> if you get the result wrong</li>
     </ul>
+    <br />
+    <strong><u>Score</u></strong>
+    For each of the teams you will be scored on how close your predicted score
+    is to the actual score. This happens twice over as you get one set of score
+    points for each team in the match. Score points are determined as follows:
+    <ul>
+      <li><strong>3 points</strong> for the exact score, <i>or</i></li>
+      <li><strong>2 points</strong> for predicting a score within 5 of the
+        actual score, <i>or</i></li>
+      <li><strong>1 points</strong> for predicting a score within 10 of the
+        actual score, <i>or</i></li>
+      <li><strong>0 points</strong> for predicting a score within 15 of the
+        actual score, <i>or</i></li>
+      <li><strong>-1 points</strong> for predicting a score within 20 of the
+        actual score, <i>or</i></li>
+      <li>... and so on losing 1 (score) point for every further 5 you are out
+        on the scoreline</li>
+    </ul>
+    <br />
+    <strong><u>Margin</u></strong>
+    There are finally points available for the difference between the two team's
+    score (or the margin of vicotry). Points work similarly to the score points
+    above:
+    <ul>
+      <li><strong>3 points</strong> for the exact margin (difference between
+        teams' scores), <i>or</i></li>
+      <li><strong>2 points</strong> for predicting a margin within 5 of the
+        actual margin, <i>or</i></li>
+      <li><strong>1 points</strong> for predicting a score within 10 of the
+        actual margin, <i>or</i></li>
+      <li><strong>0 points</strong> for predicting a score within 15 of the
+        actual margin, <i>or</i></li>
+      <li><strong>-1 points</strong> for predicting a score within 20 of the
+        actual margin, <i>or</i></li>
+      <li>... and so on losing 1 (score) point for every further 5 you are out
+        on the margin</li>
+    </ul>
+    <br />
+    It is worth noting that score points and margin points can still be gained
+    (or lost!) even when the initial result has been predicted wrong. For
+    example in a tight match where you predict one side to win by 1 point but in
+    fact the other side wins by 1 point you would still get two margin points as
+    your prediction of -1 is within 5 points of the actual +1 result
   </li>
-  <li>In the kncok-out stages it is <strong>the score after extra time</strong> that counts.
-  Penalties are ignored from the prediction and draws can therefore still be predicted</li>
+  <li>In addition to the base scores per match set out above multipliers will
+    be applied so that matches later in the tournament will earn more points.
+    The following multipliers will be applied:
+    <ul>
+      <li><i>Group Stages</i> - x1 (i.e. no multiplier)</li>
+      <li><i>Quarter Finals</i> - x2</li>
+      <li><i>Semi Finals</i> - x3</li>
+      <li><i>3<sup>rd</sup> 4<sup>th</sup> Place Play-off</i> - x1</li>
+      <li><i>Final</i> - x4</li>
+    </ul>
+  </li>
+  <li>In the kncok-out stages it is <strong>the score after extra time</strong>
+    that counts. Penalties are ignored from the prediction and draws can
+    therefore still be predicted</li>
 </ul>
 <h2>Prizes</h2>
 There will be an overall league for most points across the entire tournament
 plus three sub-leagues for the individual parts of the tournament:
 <ul>
-  <li>Group Stages (48 matches)</li>
-  <li>Last 16 Stage (8 matches)</li>
-  <li>The Rest - Quater-Finals, Semis-Finals, 3<sup>rd</sup> 4<sup>th</sup> Place Play-off & Final (8 matches)</li>
+  <li>Group Stages (40 matches)</li>
+  <li>The Rest - Quater-Finals, Semis-Finals,
+    3<sup>rd</sup> 4<sup>th</sup> Place Play-off & Final (8 matches)</li>
 </ul>
 Your sign up fee of £20 will fund a prize pool.
 This will be distributed as follows:
@@ -37,11 +97,11 @@ This will be distributed as follows:
   <tbody>
     <tr>
       <td>1<sup>st</td>
-      <td>30%</td>
+      <td>40%</td>
     </tr>
     <tr>
       <td>2<sup>nd</td>
-      <td>15%</td>
+      <td>20%</td>
     </tr>
     <tr>
       <td>3<sup>rd</td>
@@ -54,25 +114,6 @@ This will be distributed as follows:
   </tbody>
 </table>
 <h3>Group Stages</h3>
-<table class="table table-striped">
-  <thead>
-    <tr>
-      <th>Position</th>
-      <th>Prize</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>1<sup>st</td>
-      <td>10%</td>
-    </tr>
-    <tr>
-      <td>2<sup>nd</td>
-      <td>5%</td>
-    </tr>
-  </tbody>
-</table>
-<h3>Last 16</h3>
 <table class="table table-striped">
   <thead>
     <tr>
@@ -110,4 +151,5 @@ This will be distributed as follows:
     </tr>
   </tbody>
 </table>
-Prizes expressed as a percentage as I don't yet know how big the prize pool will be.
+Prizes expressed as a percentage as I don't yet know how big the prize pool will
+be.
