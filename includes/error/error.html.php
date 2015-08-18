4bd8372ca84f6f7861051fa0bf24278cc50ee166
James MacAdie (server) <james@macadie.co.uk>
2015-08-18 13:12:54 +0100

    Fix syntax error in error page
    The if clauses weren't wrapped in brackets, which causes everything to crash

--- includes/error/error.html.php
+++ includes/error/error.html.php
@@ -1,6 +1,6 @@
 <h2>Ooops. Something's gone wrong</h2>
 <p>Please copy the entire contents of this page and send it to <a href="mailto:james.macadie@telerealtrillium.com">James MacAdie</a></p>
 <div class="alert alert-danger">
-<?php if isset($error) echo('<h2>'.$error.'</h2>') ?>
-<?php if isset($errorDetail) echo('<p>'.$errorDetail.'</p>') ?>
+<?php if (isset($error)) echo('<h2>'.$error.'</h2>') ?>
+<?php if (isset($errorDetail)) echo('<p>'.$errorDetail.'</p>') ?>
 </div>
