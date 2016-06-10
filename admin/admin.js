c6c6343300a984d0aa78d36802347d91f2420232
James MacAdie <james@macadie.co.uk>
2016-06-10 11:09:06 +0100

    Client side JS for reset password

--- admin/admin.js
+++ admin/admin.js
@@ -10,6 +10,15 @@ $(document).ready(function() {
       $sel.val(),
       $sel.closest('div.row').next('div.row').find('.messageTR'));
   });
+  
+  // Add click handler to the rest user password
+  $("#accordionUsers").find('button').click(function(e) {
+    e.preventDefault();
+    var $sel = $(this).closest('form').find('select');
+    resetPassword(
+      $sel.val(),
+      $("#usersMessage"));
+  });
 
 });
 
@@ -85,6 +94,84 @@ function processUpdateTRReturn (data, $message) {
 
     // Close "submitting data" alert
     $message.html('');
+
+  }
+
+}
+
+// Function to handle resetting a user's password
+function resetPassword(userID, $message) {
+  
+  // TODO: write a dialog box to confirm really want to reset this user's password
+  
+  // Build HTML for info message
+  var result = [
+    '<div class="alert alert-info alert-dismissable">',
+    '<button type="button"',
+    ' class="close"',
+    ' data-dismiss="alert"',
+    ' aria-hidden="true">&times;</button>',
+    'Submitting update to database...',
+    '</div>'];
+
+  // Output message
+  $message.html(result.join(''));
+
+  // Make the AJAX call
+  $.ajax({
+    url: 'resetPassword.php',
+    type: 'POST',
+    data:
+      {action: "resetPassword",
+      userID: userID},
+    dataType: 'json',
+    success: function(data) {
+      //alert ('Success');
+      processResetPasswordReturn (data, $message);
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      alert ('AJAX callback error: ' + textStatus + ', ' + errorThrown);
+    }
+  });
+
+}
+
+// Callback function to process the returned data when filters are updated (or
+// page is first loaded)
+function processResetPasswordReturn (data, $message) {
+
+  if (data.result === "No") {
+  // If result returned no then something went wrong so build and display an
+  // error message
+
+    // Build HTML for error message
+    var result = [
+      '<div class="alert alert-danger alert-dismissable">',
+      '<button type="button"',
+      ' class="close"',
+      ' data-dismiss="alert"',
+      ' aria-hidden="true">&times;</button>',
+      '<h4>Reset Password Error</h4>',
+      data.message,
+      '</div>'];
+
+    // Output error message
+    $message.html(result.join(''));
+
+  } else {
+
+    // Build HTML for success message
+    var result = [
+      '<div class="alert alert-success alert-dismissable">',
+      '<button type="button"',
+      ' class="close"',
+      ' data-dismiss="alert"',
+      ' aria-hidden="true">&times;</button>',
+      'Password successfully reset!'
+      '</div>'];
+
+    // Output error message
+    $message.html(result.join(''));
 
   }
 
