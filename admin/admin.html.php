b60993a1ccf1799ff500ed4f6c27b2e0471e4c4d
James MacAdie <james@macadie.co.uk>
2016-06-10 10:51:44 +0100

    Add a form for all users
    On admin page to allow us to rest users. Doesn't do anything yet as we need to hook up some AJAX calls to it

--- admin/admin.html.php
+++ admin/admin.html.php
@@ -50,7 +50,27 @@
     </div>
     <div id="collapseUsers" class="panel-collapse collapse">
       <div class="panel-body">
-        
+      <?php if (isset($users)) : ?>
+        <div class="row">
+        <form class="form-inline" role="form">
+          <div class="col-sm-5 text-left">
+            <select class="form-control" id="userReset">
+              <option value="0" selected>- - -</option>
+            <?php foreach ($users as $row) : ?>
+              <option value = "<?php echo($row['userID']); ?>">
+                <?php echo($row['name']); ?>
+              </option>
+            <?php endforeach; ?>
+            </select>
+          </div>
+          <div class="col-sm-2 text-center">
+            <button type="submit" class="btn btn-primary">
+              Update
+            </button>
+          </div>
+        </form>
+        </div>
+      <?php endif; ?>
       </div>
     </div>
   </div>
