f971c21dca7755162bc54d651066768cfb44a2a7
James MacAdie (server name) <james@macadie.co.uk>
2020-09-13 12:03:19 +0100

    Add filters for team and Game Week
    This allows users to see a smaller subsection of matches on the fixtures
    page

--- fixtures/fixtures.html.php
+++ fixtures/fixtures.html.php
@@ -22,6 +22,424 @@
           Exclude predicted matches
           </label>
         </div>
+        <hr>
+        <h3>Pick Teams</h3>
+        <div class="panel-group" id="accordionTeam">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <a data-toggle="collapse" data-parent="#accordionTeam" href="#collapseTeam">
+                Hide / unhide
+              </a>
+            </div>
+            <div id="collapseTeam" class="panel-collapse collapse">
+              <div class="panel-body">
+                <button class="btn btn-sm btn-primary" type="submit" id="btnSelectTeam" data-mode="unselect">Unselect all</button>
+                <div class="checkbox">
+                  <label>
+                  <input type="checkbox" id="ckbT1" value="" checked>
+                  <strong>Arsenal</strong><br />
+                  </label>
+                </div>
+                <div class="checkbox">
+                  <label>
+                  <input type="checkbox" id="ckbT2" value="" checked>
+                  <strong>Aston Villa</strong><br />
+                  </label>
+                </div>
+                <div class="checkbox">
+                  <label>
+                  <input type="checkbox" id="ckbT3" value="" checked>
+                  <strong>Brighton &amp; Hove Albion</strong><br />
+                  </label>
+                </div>
+                <div class="checkbox">
+                  <label>
+                  <input type="checkbox" id="ckbT4" value="" checked>
+                  <strong>Burnley</strong><br />
+                  </label>
+                </div>
+                <div class="checkbox">
+                  <label>
+                  <input type="checkbox" id="ckbT5" value="" checked>
+                  <strong>Chelsea</strong><br />
+                  </label>
+                </div>
+                <div class="checkbox">
+                  <label>
+                  <input type="checkbox" id="ckbT6" value="" checked>
+                  <strong>Crystal Palace</strong><br />
+                  </label>
+                </div>
+                <div class="checkbox">
+                  <label>
+                  <input type="checkbox" id="ckbT7" value="" checked>
+                  <strong>Everton</strong><br />
+                  </label>
+                </div>
+                <div class="checkbox">
+                  <label>
+                  <input type="checkbox" id="ckbT8" value="" checked>
+                  <strong>Fulham</strong><br />
+                  </label>
+                </div>
+                <div class="checkbox">
+                  <label>
+                  <input type="checkbox" id="ckbT9" value="" checked>
+                  <strong>Leeds United</strong><br />
+                  </label>
+                </div>
+                <div class="checkbox">
+                  <label>
+                  <input type="checkbox" id="ckbT10" value="" checked>
+                  <strong>Leicester City</strong><br />
+                  </label>
+                </div>
+                <div class="checkbox">
+                  <label>
+                  <input type="checkbox" id="ckbT11" value="" checked>
+                  <strong>Liverpool</strong><br />
+                  </label>
+                </div>
+                <div class="checkbox">
+                  <label>
+                  <input type="checkbox" id="ckbT12" value="" checked>
+                  <strong>Manchester City</strong><br />
+                  </label>
+                </div>
+                <div class="checkbox">
+                  <label>
+                  <input type="checkbox" id="ckbT13" value="" checked>
+                  <strong>Manchester United</strong><br />
+                  </label>
+                </div>
+                <div class="checkbox">
+                  <label>
+                  <input type="checkbox" id="ckbT14" value="" checked>
+                  <strong>Newcastle United</strong><br />
+                  </label>
+                </div>
+                <div class="checkbox">
+                  <label>
+                  <input type="checkbox" id="ckbT15" value="" checked>
+                  <strong>Sheffield United</strong><br />
+                  </label>
+                </div>
+                <div class="checkbox">
+                  <label>
+                  <input type="checkbox" id="ckbT16" value="" checked>
+                  <strong>Southampton</strong><br />
+                  </label>
+                </div>
+                <div class="checkbox">
+                  <label>
+                  <input type="checkbox" id="ckbT17" value="" checked>
+                  <strong>Tottenham Hotspur</strong><br />
+                  </label>
+                </div>
+                <div class="checkbox">
+                  <label>
+                  <input type="checkbox" id="ckbT18" value="" checked>
+                  <strong>West Bromwich Albion</strong><br />
+                  </label>
+                </div>
+                <div class="checkbox">
+                  <label>
+                  <input type="checkbox" id="ckbT19" value="" checked>
+                  <strong>West Ham United</strong><br />
+                  </label>
+                </div>
+                <div class="checkbox">
+                  <label>
+                  <input type="checkbox" id="ckbT20" value="" checked>
+                  <strong>Wovlerhampton Wanderers</strong><br />
+                  </label>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <hr>
+        <h3>Select Game Week</h3>
+        <div class="panel-group" id="accordionWeek">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <a data-toggle="collapse" data-parent="#accordionWeek" href="#collapseWeek">
+                Hide / unhide
+              </a>
+            </div>
+            <div id="collapseWeek" class="panel-collapse collapse">
+              <div class="panel-body">
+                <button class="btn btn-sm btn-primary" type="submit" id="btnSelectWeek" data-mode="unselect">Unselect all</button>
+                <div class="checkbox">
+                  <label>
+                  <input type="checkbox" id="ckbGW1" value="" checked>
+                  <strong>Game Week 1</strong><br />
+                  12<sup>th</sup> September 2020 - 14<sup>th</sup> September 2020
+                  </label>
+                </div>
+                <div class="checkbox">
+                  <label>
+                  <input type="checkbox" id="ckbGW2" value="" checked>
+                  <strong>Game Week 2</strong><br />
+                  19<sup>th</sup> September 2020 - 21<sup>st</sup> September 2020
+                  </label>
+                </div>
+                <div class="checkbox">
+                  <label>
+                  <input type="checkbox" id="ckbGW3" value="" checked>
+                  <strong>Game Week 3</strong><br />
+                  26<sup>th</sup> September 2020 - 28<sup>th</sup> September 2020
+                  </label>
+                </div>
+                <div class="checkbox">
+                  <label>
+                  <input type="checkbox" id="ckbGW4" value="" checked>
+                  <strong>Game Week 4</strong><br />
+                  3<sup>rd</sup> October 2020 - 3<sup>rd</sup> October 2020
+                  </label>
+                </div>
+                <div class="checkbox">
+                  <label>
+                  <input type="checkbox" id="ckbGW5" value="" checked>
+                  <strong>Game Week 5</strong><br />
+                  17<sup>th</sup> October 2020 - 17<sup>th</sup> October 2020
+                  </label>
+                </div>
+                <div class="checkbox">
+                  <label>
+                  <input type="checkbox" id="ckbGW6" value="" checked>
+                  <strong>Game Week 6</strong><br />
+                  24<sup>th</sup> October 2020 - 24<sup>st</sup> October 2020
+                  </label>
+                </div>
+                <div class="checkbox">
+                  <label>
+                  <input type="checkbox" id="ckbGW7" value="" checked>
+                  <strong>Game Week 7</strong><br />
+                  31<sup>st</sup> October 2020 - 31<sup>st</sup> October 2020
+                  </label>
+                </div>
+                <div class="checkbox">
+                  <label>
+                  <input type="checkbox" id="ckbGW8" value="" checked>
+                  <strong>Game Week 8</strong><br />
+                  7<sup>th</sup> November 2020 - 7<sup>th</sup> November 2020
+                  </label>
+                </div>
+                <div class="checkbox">
+                  <label>
+                  <input type="checkbox" id="ckbGW9" value="" checked>
+                  <strong>Game Week 9</strong><br />
+                  21<sup>st</sup> November 2020 - 21<sup>st</sup> November 2020
+                  </label>
+                </div>
+                <div class="checkbox">
+                  <label>
+                  <input type="checkbox" id="ckbGW10" value="" checked>
+                  <strong>Game Week 10</strong><br />
+                  28<sup>th</sup> November 2020 - 28<sup>th</sup> November 2020
+                  </label>
+                </div>
+                <div class="checkbox">
+                  <label>
+                  <input type="checkbox" id="ckbGW11" value="" checked>
+                  <strong>Game Week 11</strong><br />
+                  5<sup>th</sup> December 2020 - 5<sup>th</sup> December 2020
+                  </label>
+                </div>
+                <div class="checkbox">
+                  <label>
+                  <input type="checkbox" id="ckbGW12" value="" checked>
+                  <strong>Game Week 12</strong><br />
+                  12<sup>th</sup> December 2020 - 13<sup>th</sup> December 2020
+                  </label>
+                </div>
+                <div class="checkbox">
+                  <label>
+                  <input type="checkbox" id="ckbGW13" value="" checked>
+                  <strong>Game Week 13</strong><br />
+                  15<sup>th</sup> December 2020 - 16<sup>th</sup> December 2020
+                  </label>
+                </div>
+                <div class="checkbox">
+                  <label>
+                  <input type="checkbox" id="ckbGW14" value="" checked>
+                  <strong>Game Week 14</strong><br />
+                  19<sup>th</sup> December 2020 - 19<sup>th</sup> December 2020
+                  </label>
+                </div>
+                <div class="checkbox">
+                  <label>
+                  <input type="checkbox" id="ckbGW15" value="" checked>
+                  <strong>Game Week 15</strong><br />
+                  26<sup>th</sup> December 2020 - 26<sup>th</sup> December 2020
+                  </label>
+                </div>
+                <div class="checkbox">
+                  <label>
+                  <input type="checkbox" id="ckbGW16" value="" checked>
+                  <strong>Game Week 16</strong><br />
+                  28<sup>th</sup> December 2020 - 28<sup>th</sup> December 2020
+                  </label>
+                </div>
+                <div class="checkbox">
+                  <label>
+                  <input type="checkbox" id="ckbGW17" value="" checked>
+                  <strong>Game Week 17</strong><br />
+                  2<sup>nd</sup> January 2021 - 2<sup>nd</sup> January 2021
+                  </label>
+                </div>
+                <div class="checkbox">
+                  <label>
+                  <input type="checkbox" id="ckbGW18" value="" checked>
+                  <strong>Game Week 18</strong><br />
+                  12<sup>th</sup> January 2021 - 13<sup>th</sup> January 2021
+                  </label>
+                </div>
+                <div class="checkbox">
+                  <label>
+                  <input type="checkbox" id="ckbGW19" value="" checked>
+                  <strong>Game Week 19</strong><br />
+                  16<sup>th</sup> January 2021 - 16<sup>th</sup> January 2021
+                  </label>
+                </div>
+                <div class="checkbox">
+                  <label>
+                  <input type="checkbox" id="ckbGW20" value="" checked>
+                  <strong>Game Week 20</strong><br />
+                  26<sup>th</sup> January 2021 - 27<sup>th</sup> January 2021
+                  </label>
+                </div>
+                <div class="checkbox">
+                  <label>
+                  <input type="checkbox" id="ckbGW21" value="" checked>
+                  <strong>Game Week 21</strong><br />
+                  30<sup>th</sup> January 2021 - 30<sup>th</sup> January 2021
+                  </label>
+                </div>
+                <div class="checkbox">
+                  <label>
+                  <input type="checkbox" id="ckbGW22" value="" checked>
+                  <strong>Game Week 22</strong><br />
+                  2<sup>nd</sup> February 2021 - 3<sup>rd</sup> February 2021
+                  </label>
+                </div>
+                <div class="checkbox">
+                  <label>
+                  <input type="checkbox" id="ckbGW23" value="" checked>
+                  <strong>Game Week 23</strong><br />
+                  6<sup>th</sup> February 2021 - 6<sup>th</sup> February 2021
+                  </label>
+                </div>
+                <div class="checkbox">
+                  <label>
+                  <input type="checkbox" id="ckbGW24" value="" checked>
+                  <strong>Game Week 24</strong><br />
+                  13<sup>th</sup> February 2021 - 13<sup>th</sup> February 2021
+                  </label>
+                </div>
+                <div class="checkbox">
+                  <label>
+                  <input type="checkbox" id="ckbGW25" value="" checked>
+                  <strong>Game Week 25</strong><br />
+                  20<sup>th</sup> February 2021 - 20<sup>th</sup> February 2021
+                  </label>
+                </div>
+                <div class="checkbox">
+                  <label>
+                  <input type="checkbox" id="ckbGW26" value="" checked>
+                  <strong>Game Week 26</strong><br />
+                  27<sup>th</sup> February 2021 - 27<sup>th</sup> February 2021
+                  </label>
+                </div>
+                <div class="checkbox">
+                  <label>
+                  <input type="checkbox" id="ckbGW27" value="" checked>
+                  <strong>Game Week 27</strong><br />
+                  6<sup>th</sup> March 2021 - 6<sup>st</sup> March 2021
+                  </label>
+                </div>
+                <div class="checkbox">
+                  <label>
+                  <input type="checkbox" id="ckbGW28" value="" checked>
+                  <strong>Game Week 28</strong><br />
+                  13<sup>th</sup> March 2021 - 13<sup>th</sup> March 2021
+                  </label>
+                </div>
+                <div class="checkbox">
+                  <label>
+                  <input type="checkbox" id="ckbGW29" value="" checked>
+                  <strong>Game Week 29</strong><br />
+                  20<sup>th</sup> March 2021 - 20<sup>th</sup> March 2021
+                  </label>
+                </div>
+                <div class="checkbox">
+                  <label>
+                  <input type="checkbox" id="ckbGW30" value="" checked>
+                  <strong>Game Week 30</strong><br />
+                  3<sup>rd</sup> April 2021 - 3<sup>rd</sup> April 2021
+                  </label>
+                </div>
+                <div class="checkbox">
+                  <label>
+                  <input type="checkbox" id="ckbGW31" value="" checked>
+                  <strong>Game Week 31</strong><br />
+                  10<sup>th</sup> April 2021 - 10<sup>th</sup> April 2021
+                  </label>
+                </div>
+                <div class="checkbox">
+                  <label>
+                  <input type="checkbox" id="ckbGW32" value="" checked>
+                  <strong>Game Week 32</strong><br />
+                  17<sup>th</sup> April 2021 - 17<sup>th</sup> April 2021
+                  </label>
+                </div>
+                <div class="checkbox">
+                  <label>
+                  <input type="checkbox" id="ckbGW33" value="" checked>
+                  <strong>Game Week 33</strong><br />
+                  24<sup>th</sup> April 2021 - 24<sup>th</sup> April 2021
+                  </label>
+                </div>
+                <div class="checkbox">
+                  <label>
+                  <input type="checkbox" id="ckbGW34" value="" checked>
+                  <strong>Game Week 34</strong><br />
+                  1<sup>st</sup> May 2021 - 1<sup>st</sup> May 2021
+                  </label>
+                </div>
+                <div class="checkbox">
+                  <label>
+                  <input type="checkbox" id="ckbGW35" value="" checked>
+                  <strong>Game Week 35</strong><br />
+                  8<sup>th</sup> May 2021 - 8<sup>th</sup> May 2021
+                  </label>
+                </div>
+                <div class="checkbox">
+                  <label>
+                  <input type="checkbox" id="ckbGW36" value="" checked>
+                  <strong>Game Week 36</strong><br />
+                  11<sup>th</sup> May 2021 - 12<sup>th</sup> May 2021
+                  </label>
+                </div>
+                <div class="checkbox">
+                  <label>
+                  <input type="checkbox" id="ckbGW37" value="" checked>
+                  <strong>Game Week 37</strong><br />
+                  15<sup>th</sup> May 2021 - 15<sup>th</sup> May 2021
+                  </label>
+                </div>
+                <div class="checkbox">
+                  <label>
+                  <input type="checkbox" id="ckbGW38" value="" checked>
+                  <strong>Game Week 38</strong><br />
+                  23<sup>rd</sup> May 2021 - 23<sup>rd</sup> May 2021
+                  </label>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
         <button class="btn btn-lg btn-primary btn-block" type="submit" id="btnUpdateMatches">Update</button>
       </div>
     </div>
--- fixtures/fixtures.js
+++ fixtures/fixtures.js
@@ -12,9 +12,22 @@ $(document).ready(function() {
   $("#btnUpdateMatches").click(function(e) {
     e.preventDefault();
     $("#collapseFilters").collapse('hide'); // collapse the filters section
-    $("#collapseGroup").collapse('hide'); // collapse the filters section
+    $("#collapseTeam").collapse('hide'); // collapse the filters section
+    $("#collapseWeek").collapse('hide'); // collapse the filters section
     window.scrollTo(0, 0);
     getMatchesData();
+  });
+
+  // Add click handler to select / unselect all the teams button
+  $("#btnSelectTeam").click(function(e) {
+    e.preventDefault();
+    toggleSelect($(this).attr('data-mode'), $('#collapseTeam'), $(this));
+  });
+
+  // Add click handler to select / unselect all game weeks button
+  $("#btnSelectWeek").click(function(e) {
+    e.preventDefault();
+    toggleSelect($(this).attr('data-mode'), $('#collapseWeek'), $(this));
   });
 
 });
@@ -44,7 +57,65 @@ function getMatchesData() {
     data:
       {action: "updateMatches",
       excPlayed: $("#ckbPlayedMatches").is(':checked'),
-      excPredicted: $("#ckbPredictedMatches").is(':checked')},
+      excPredicted: $("#ckbPredictedMatches").is(':checked'),
+      t1: $('#ckbT1').is(':checked'),
+      t2: $('#ckbT2').is(':checked'),
+      t3: $('#ckbT3').is(':checked'),
+      t4: $('#ckbT4').is(':checked'),
+      t5: $('#ckbT5').is(':checked'),
+      t6: $('#ckbT6').is(':checked'),
+      t7: $('#ckbT7').is(':checked'),
+      t8: $('#ckbT8').is(':checked'),
+      t9: $('#ckbT9').is(':checked'),
+      t10: $('#ckbT10').is(':checked'),
+      t11: $('#ckbT11').is(':checked'),
+      t12: $('#ckbT12').is(':checked'),
+      t13: $('#ckbT13').is(':checked'),
+      t14: $('#ckbT14').is(':checked'),
+      t15: $('#ckbT15').is(':checked'),
+      t16: $('#ckbT16').is(':checked'),
+      t17: $('#ckbT17').is(':checked'),
+      t18: $('#ckbT18').is(':checked'),
+      t19: $('#ckbT19').is(':checked'),
+      t20: $('#ckbT20').is(':checked'),
+      gw1: $('#ckbGW1').is(':checked'),
+      gw2: $('#ckbGW2').is(':checked'),
+      gw3: $('#ckbGW3').is(':checked'),
+      gw4: $('#ckbGW4').is(':checked'),
+      gw5: $('#ckbGW5').is(':checked'),
+      gw6: $('#ckbGW6').is(':checked'),
+      gw7: $('#ckbGW7').is(':checked'),
+      gw8: $('#ckbGW8').is(':checked'),
+      gw9: $('#ckbGW9').is(':checked'),
+      gw10: $('#ckbGW10').is(':checked'),
+      gw11: $('#ckbGW11').is(':checked'),
+      gw12: $('#ckbGW12').is(':checked'),
+      gw13: $('#ckbGW13').is(':checked'),
+      gw14: $('#ckbGW14').is(':checked'),
+      gw15: $('#ckbGW15').is(':checked'),
+      gw16: $('#ckbGW16').is(':checked'),
+      gw17: $('#ckbGW17').is(':checked'),
+      gw18: $('#ckbGW18').is(':checked'),
+      gw19: $('#ckbGW19').is(':checked'),
+      gw20: $('#ckbGW20').is(':checked'),
+      gw21: $('#ckbGW21').is(':checked'),
+      gw22: $('#ckbGW22').is(':checked'),
+      gw23: $('#ckbGW23').is(':checked'),
+      gw24: $('#ckbGW24').is(':checked'),
+      gw25: $('#ckbGW25').is(':checked'),
+      gw26: $('#ckbGW26').is(':checked'),
+      gw27: $('#ckbGW27').is(':checked'),
+      gw28: $('#ckbGW28').is(':checked'),
+      gw29: $('#ckbGW29').is(':checked'),
+      gw30: $('#ckbGW30').is(':checked'),
+      gw31: $('#ckbGW31').is(':checked'),
+      gw32: $('#ckbGW32').is(':checked'),
+      gw33: $('#ckbGW33').is(':checked'),
+      gw34: $('#ckbGW34').is(':checked'),
+      gw35: $('#ckbGW35').is(':checked'),
+      gw36: $('#ckbGW36').is(':checked'),
+      gw37: $('#ckbGW37').is(':checked'),
+      gw38: $('#ckbGW38').is(':checked')},
     dataType: 'json',
     success: function(data) {
       //alert ('Success');
@@ -189,6 +260,23 @@ function processMatchesReturn (data) {
     // Close "Getting data" alert
     $("#aGettingMatchData").alert('close');
 
+  }
+
+}
+
+function toggleSelect (mode, section, button) {
+
+  // see if we're selecting or unselecting
+  if (mode === 'unselect') {
+    // Change all the checkbox states to unchecked
+    section.find('[type=checkbox]').prop('checked',false);
+    // Change button text back to select all
+    button.text('Select All').attr('data-mode','select');
+  } else {
+    // Change all the checkbox states to checked
+    section.find('[type=checkbox]').prop('checked',true);
+    // Change button text back to unselect all
+    button.text('Unselect All').attr('data-mode','unselect');
   }
 
 }
