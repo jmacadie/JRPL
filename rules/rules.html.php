64fdf2e956b4523642dcf2d83de878fdb51bd938
James MacAdie (server name) <james@macadie.co.uk>
2021-06-08 08:58:20 +0100

    Update Rules page
    Still had rugby scoring

--- rules/rules.html.php
+++ rules/rules.html.php
@@ -2,149 +2,109 @@
 <div class="alert alert-info">I've got a four lettered word for you...</div>
 <h2>How does it work?</h2>
 <ul>
-  <li>Predict the exact score in every match in the 2015 Rugby World Cup</li>
+  <li>Predict the exact score in every match in the 2021 Euros</li>
   <li>Change your prediction as many times as you like before the match</li>
   <li>Matches will be locked down from further prediction
     <strong>30 minutes</strong> before kick-off</li>
   <li>The system will send an e-mail at lock-down saying what every player
-    predicted for that match. Note that the e-mail may not go out exactly half
-    an hour before the kick-off but when the first person tries to interact with
-    the website after the lock-down point. This ensures that it is not possible
-    to illegally tamper with your prediction after the lock-down</li>
+    predicted for that match<br />
+  <small>Note that the e-mail may not go out exactly half an hour before the
+  kick-off but when the first person tries to interact with the website after
+  the lock-down point. This ensures that it is not possible to illegally
+  tamper with your prediction after the lock-down</small></li>
+  <li>You will not know anyone else's predictions before lock-down, which allows
+    all players to make predictions without any possibility of gaming a prediction
+  relative to someone else. Disclosure of predictions before lock-down is a
+  serious offense, so don't do it</li>
+  <li>It is essential that you use these 30 minutes between lock-down and kick-off
+    to riducule other players' predictions, preferably in public</li>
   <li>After the match is over, and someone has posted the result, the
-    predictions will be scored with two different scoring systems (Standard and
-    Auto-Quiz) and a further e-mail will be sent out</li>
+    predictions will be scored and a further e-mail will be sent out</li>
   <li>In the knock-out stages it is <strong>the score after extra time and
-    sudden death (as played)</strong> that counts. The kicking competition is
+    sudden death (if played)</strong> that counts. Penalty shoot outs are
     ignored from the result and draws can therefore still be predicted</li>
   <li>It costs &pound;20 to participate and this will fund prizes for the best
     predictors</li>
 </ul>
 <h2>Standard Scoring System</h2>
-  <p>There are JRPL points available over 4 different scoring strands<p>
-  <strong><u>Result</u></strong>
-  <ul>
-    <li><strong>3 points</strong> for the correct result (e.g. England Win,
-      Draw, Fiji Win)</li>
-    <li><strong>0 points</strong> if you get the result wrong</li>
-  </ul>
-  <br />
-  <strong><u>Score</u></strong>
-  For each of the teams you will be scored on how close your predicted score
-  is to the actual score. This happens twice over as you get one set of score
-  points for each team in the match. Score points are determined as follows:
-  <ul>
-    <li><strong>3 points</strong> for the exact score, <i>or</i></li>
-    <li><strong>2 points</strong> for predicting a score within 5 of the
-      actual score, <i>or</i></li>
-    <li><strong>1 point</strong> for predicting a score within 10 of the
-      actual score, <i>or</i></li>
-    <li><strong>0 points</strong> for predicting a score within 15 of the
-      actual score, <i>or</i></li>
-    <li><strong>-1 points</strong> for predicting a score within 20 of the
-      actual score, <i>or</i></li>
-    <li>... and so on losing 1 (score) point for every further 5 you are out
-      on the scoreline</li>
-  </ul>
-  <br />
-  <strong><u>Margin</u></strong>
-  There are finally points available for the difference between the two team's
-  score (or the margin of victory). Points work similarly to the score points
-  above:
-  <ul>
-    <li><strong>3 points</strong> for the exact margin (difference between
-      teams' scores), <i>or</i></li>
-    <li><strong>2 points</strong> for predicting a margin within 5 of the
-      actual margin, <i>or</i></li>
-    <li><strong>1 point</strong> for predicting a score within 10 of the
-      actual margin, <i>or</i></li>
-    <li><strong>0 points</strong> for predicting a score within 15 of the
-      actual margin, <i>or</i></li>
-    <li><strong>-1 points</strong> for predicting a score within 20 of the
-      actual margin, <i>or</i></li>
-    <li>... and so on losing 1 (margin) point for every further 5 you are out
-      on the margin</li>
-  </ul>
-  <p>It is worth noting that score points and margin points can still be gained
-  (or lost!) even when the initial result has been predicted wrong. For
-  example in a tight match where you predict one side to win by 1 point but in
-  fact the other side wins by 1 point you would still get two margin points as
-  your prediction of -1 is within 5 points of the actual +1 margin</p>
-  <strong><u>Multipliers</u></strong> In addition to the base scores per match,
-  set out above, multipliers will be applied so that matches later in the
-  tournament will earn more points.  The following multipliers will be applied:
-  <ul>
-    <li>Group Stages - <i>x</i>1 (i.e. no multiplier)</li>
-    <li>Quarter Finals - <i>x</i>2</li>
-    <li>Semi Finals - <i>x</i>3</li>
-    <li>3<sup>rd</sup> 4<sup>th</sup> Place Play-off - <i>x</i>1</li>
-    <li>Final - <i>x</i>4</li>
-  </ul>
-  <p>If you fail to input a prediction for any given match, the system will
-  assume you've entered a 0 - 0 prediction. However, should that prediction lead
-  to any positive points (on any scoring strand), then your score (on that
-  strand) will be capped at zero. In practice this means you will get either no
-  points (best case) or, more likely, lose points should any one of the scoring
-  strands give rise to a negative points score</p>
-</li>
+<p>We're running vanilla JRPL rules this year. There are <u>no muplipliers to
+the knock-out stages</u></p>
+<p>So points awarded as follows:</p>
+<ul>
+  <li>
+    <p><strong>3 Points</strong> for exactly the right scorline predicted<br />
+  <small>e.g. Turkey to beat Italy 2 - 1 and then that's what happens</small></p>
+  </li>
+  <li>
+    <p><strong>1 Point</strong> for the wrong scoreline, but the right result<br />
+  <small>e.g. Turkey to beat Italy 2 - 1 but then they win by any other scoreline</small></p>
+  </li>
+  <li>
+    <p><strong>No Points</strong> for the wrong result<br />
+  <small>e.g. Turkey to beat Italy 2 - 1 but it's a draw or Italy win</small></p>
+  </li>
+</ul>
 <h2>Auto-Quiz Scoring System</h2>
 <p>After much debate (and bemusement) we settled on the following system:</p>
+<p><small>Note this scetions is still TBD by the rules committee. Feeling run strong in this area</small></p>
 <ul>
   <li>Every prediction is measured for "goodness" by taking its Euclidean
     distance from the result</li>
+  <li>Since result is important, as well as home team score and away team score,
+    which define a 2D space, the result will mapped on the third dimension. Home
+  team win will be worth 0, a draw 3 and away team win 6. Euclidean distance
+  will then be measured in 3D space</li>
   <li>Everyone's Euclidean distance (ED) is inverted by subtracting each player's
     ED from the player with the biggest (i.e. worst) ED</li>
-  <li>There is a fixed pool of points available for each match being 10 points
-    for each player that makes a prediction. Note that this means if people do
-    not predict in a given match, it reduces the avialable points for everyone
-    else</li>
+  <li>There is a fixed pool of 100 points available for each match</li>
   <li>The fixed pool of points is then allocated to each player proportionate to
     their inverted ED score. This means the player with the worst prediction
     will always get no Auto-Quiz points in the match</li>
-  <li>If you miss a prediction (as well as reducing the available points pot to
-    be allocated for that match) you will always get zero Auto-Quiz points</li>
-  <li>There are no multipliers in Auto-Quiz</li>
+  <li>If you miss a prediction you will always get zero Auto-Quiz points</li>
 </ul>
 <h2>Prizes</h2>
 <p>There will be an overall league for most points across the entire tournament
 plus two sub-leagues for the individual parts of the tournament:
 <ul>
-  <li>Group Stages (40 matches)</li>
-  <li>The Rest: Quarter-Finals, Semis-Finals,
-    3<sup>rd</sup> 4<sup>th</sup> Place Play-off & Final (8 matches)</li>
+  <li>Group Stages (36 matches)</li>
+  <li>The Rest: Round of 16, Quarter-Finals, Semis-Finals & Final (15 matches)</li>
 </ul></p>
-Your sign up fee of &pound;20 will fund a prize pool. 50% of the prizes will be
-awarded based on Standard points scores and the other 50% based on Auto-Quiz.
+Your sign up fee of &pound;20 will fund a prize pool.
 The pool will be distributed as follows:
 <h3>Overall</h3>
 <table class="table table-striped">
   <thead>
     <tr>
       <th>Position</th>
-      <th>Standrd Prize</th>
-      <th>Auto-Quiz Prize</th>
+      <th>Standard</th>
+    <th>Auto-Quiz</th>
     </tr>
   </thead>
   <tbody>
     <tr>
       <td>1<sup>st</td>
-      <td>20%</td>
-      <td>20%</td>
+      <td>12.5%</td>
+      <td>12.5%</td>
     </tr>
     <tr>
       <td>2<sup>nd</td>
-      <td>10%</td>
-      <td>10%</td>
+      <td>8.75%</td>
+      <td>8.75%</td>
     </tr>
     <tr>
       <td>3<sup>rd</td>
       <td>5%</td>
       <td>5%</td>
     </tr>
+  <tr>
+      <td>21<sup>st</td>
+      <td>2.5%</td>
+      <td>2.5%</td>
+    </tr>
     <tr>
       <td>Kov</td>
       <td>Nasty</td>
-      <td>Nasty</td>
+    <td></td>
     </tr>
   </tbody>
 </table>
@@ -153,20 +113,20 @@ The pool will be distributed as follows:
   <thead>
     <tr>
       <th>Position</th>
-      <th>Standrd Prize</th>
-      <th>Auto-Quiz Prize</th>
+      <th>Standard</th>
+    <th>Auto-Quiz</th>
     </tr>
   </thead>
   <tbody>
     <tr>
       <td>1<sup>st</td>
-      <td>5%</td>
-      <td>5%</td>
+      <td>2.5%</td>
+      <td>2.5%</td>
     </tr>
     <tr>
       <td>2<sup>nd</td>
-      <td>2.5%</td>
-      <td>2.5%</td>
+      <td>1.25%</td>
+      <td>1.25%</td>
     </tr>
   </tbody>
 </table>
@@ -175,22 +135,28 @@ The pool will be distributed as follows:
   <thead>
     <tr>
       <th>Position</th>
-      <th>Standrd Prize</th>
-      <th>Auto-Quiz Prize</th>
+      <th>Standard</th>
+    <th>Auto-Quiz</th>
     </tr>
   </thead>
   <tbody>
     <tr>
       <td>1<sup>st</td>
-      <td>5%</td>
-      <td>5%</td>
+      <td>6.25%</td>
+      <td>6.25%</td>
     </tr>
     <tr>
       <td>2<sup>nd</td>
+      <td>5%</td>
+      <td>5%</td>
+    </tr>
+  <tr>
+      <td>3<sup>rd</td>
       <td>2.5%</td>
       <td>2.5%</td>
     </tr>
   </tbody>
 </table>
-Prizes expressed as a percentage as I don't yet know how big the prize pool will
-be.
+<p>The final 8% of the prize pool will be donated to charity</p>
+<p>Prizes expressed as a percentage as I don't yet know how big the prize pool will
+be.</p>
